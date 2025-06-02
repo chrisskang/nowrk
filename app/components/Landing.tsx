1d2edb2c496698c08a7b9f046aa5c2fe65e92bff
chrisskang <chris.kangaroo@icloud.com>
2025-06-02 02:05:53 +0200

    test2

--- app/components/Landing.tsx
+++ app/components/Landing.tsx
@@ -6,7 +6,7 @@ const Landing = () => {
     <div>
       <BackgroundAnimation />
       <div className="z-10 w-full max-w-md text-center ">
-        <h1 className="text-4xl font-bold mb-4 text-gray-800">Coming Soon</h1>
+        <h1 className="text-4xl font-bold mb-4 text-gray-800">Coming Soon!</h1>
         <p className="text-xl mb-8 text-gray-600">
           We're working hard to bring you something amazing. Stay tuned!
         </p>
